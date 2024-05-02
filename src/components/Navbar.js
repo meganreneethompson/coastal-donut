c1739a6631214ac8a634790166bc8570d9fdd1d3
Meg Thompson <128401836+meganreneethompson@users.noreply.github.com>
2024-05-02 10:17:50 -0400

    update to paragrah

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <div className='navbar-container'>
+<Link to="/" className="navbar-logo"></Link>
+TRVL 
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
